79c64a4dce1fd911fa1c6cabc3e0ec75883c84e4
mr.mekambe <grubyogon10@gmail.com>
2024-09-07 23:49:08 +0200

    removed graal

--- Linux-64-Start-c2colorpicker.sh
+++ Linux-64-Start-c2colorpicker.sh
@@ -4,7 +4,7 @@ if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
 fi
 
 export DIR="$(dirname "$(readlink -f "$0")")"  # Get the script's directory
-#export javaexec="$DIR/resources/jdk-17.0.8+7/bin/java"
-export javaexecgraal=$(find "$DIR/resources" -type f -name 'java' | grep 'graalvm-jdk-17')
+export javaexec="$DIR/resources/jdk-17.0.8+7/bin/java"
+#export javaexecgraal=$(find "$DIR/resources" -type f -name 'java' | grep 'graalvm-jdk-17')
 
-"$javaexecgraal" -cp cultris2.jar ColorPicker
+"$javaexec" -cp cultris2.jar ColorPicker
--- Linux-64-Start-cultris2.sh
+++ Linux-64-Start-cultris2.sh
@@ -5,7 +5,7 @@ export DIR="$(dirname "$(readlink -f "$0")")"  # Get the script's directory
 export javaexec=$(find "$DIR/resources" -type f -name 'java' | grep 'jdk-17' | grep -v 'graalvm')
 
 # Find the Java executable for GraalVM JDK-17
-export javaexecgraal=$(find "$DIR/resources" -type f -name 'java' | grep 'graalvm-jdk-17')
+#export javaexecgraal=$(find "$DIR/resources" -type f -name 'java' | grep 'graalvm-jdk-17')
 
 # Convert absolute paths to relative for output
 javaexec_rel="${javaexec#$DIR/}"
@@ -13,13 +13,14 @@ javaexecgraal_rel="${javaexecgraal#$DIR/}"
 
 # Check user input to select the legacy Java version
 if [[ "$1" == "legacy" ]]; then
-    java_to_use=$javaexec
-    java_to_use_rel=$javaexec_rel
-    echo "Using legacy JDK Java executable at: $java_to_use_rel"
-else
-    java_to_use=$javaexecgraal
+        java_to_use=$javaexecgraal
     java_to_use_rel=$javaexecgraal_rel
     echo "Using GraalVM Java executable by default at: $java_to_use_rel"
+#doesnt use graalvm
+else
+java_to_use=$javaexec
+    java_to_use_rel=$javaexec_rel
+    echo "Using legacy JDK Java executable at: $java_to_use_rel"
 fi
 
 cd "$DIR"
