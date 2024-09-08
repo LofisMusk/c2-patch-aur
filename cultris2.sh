c1e3a938fa871611d5a806be90152647763cc5d8
mr.mekambe <grubyogon10@gmail.com>
2024-09-08 19:16:36 +0200

    update

--- cultris2.sh
+++ cultris2.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 export DIR= "/opt/cultris2"  # Get the script's directory
-
-# Find the Java executable for JDK-17, excluding GraalVM versions
-export javaexec=$(find "$DIR/libs" -type f -name 'java' | grep 'jdk-17')
-
-# Convert absolute paths to relative for output
-javaexec_rel="${javaexec#$DIR/}"
-
-# Check user input to select the legacy Java version
-if [[ "$1" == "legacy" ]]; then
-echo "this is not what youre supposed to see!"
-#doesnt use graalvm
-else
-java_to_use=$javaexec
-    java_to_use_rel=$javaexec_rel
-    echo "Using legacy JDK Java executable at: $java_to_use_rel"
-fi
-
-cd "$DIR"
-
-# Check if the chosen Java executable is found and launch the application
-if [ -f "$java_to_use" ]; then
-    "$java_to_use" -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
-else
-    echo "Java executable not found. Please ensure you've installed a JDK or GraalVM correctly in the resources directory."
-fi
+java -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
