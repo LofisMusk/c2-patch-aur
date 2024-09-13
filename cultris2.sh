a7b9ca902803078d9a10153f058cc9e8e086dce2
Mekambe <grubyogon10@gmail.com>
2024-09-13 22:46:19 +0200

    removed useless libs

--- cultris2.sh
+++ cultris2.sh
@@ -2,4 +2,4 @@
 export DIR="/opt/cultris2/"
 JAVA_HOME="/usr/lib/jvm/java-17-temurin"
 
-env -C $DIR java -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
+env -C $DIR "$JAVA_HOME/bin/java" -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
