c850dd4272fdfacd8f65990ae27ad6e00b589a6d
bluetail <a-development+gitea@posteo.de>
2024-09-17 22:24:11 +0200

    try enforcing a policy as an attempt to prevent system changes

--- cultris2.sh
+++ cultris2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Set the directory and Java home environment variables
 export DIR="/opt/cultris2/"
 JAVA_HOME="/usr/lib/jvm/java-17-temurin"
+SECURITY_POLICY="$DIR/cultris2.policy"
 
-env -C $DIR "$JAVA_HOME/bin/java" -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
+# Execute the Java application from the specified directory with security policy enabled
+env -C "$DIR" "$JAVA_HOME/bin/java" \
+    -Djava.library.path="$DIR/libs/" \
+    -Djava.security.manager \
+    -Djava.security.policy="$SECURITY_POLICY" \
+    -jar "$DIR/cultris2.jar"
