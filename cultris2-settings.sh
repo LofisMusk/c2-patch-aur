3d5968ba207bf1efc4ab373b17d6ae6d75d5b28b
LofisMusk <grubyogon10@gmail.com>
2024-11-23 18:07:57 +0100

    added new commit / sorted the files

--- cultris2-settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
-    export _JAVA_AWT_WM_NONREPARENTING=1
-fi
-JAVA_HOME="/usr/lib/jvm/java-17-temurin"
-export DIR="/opt/cultris2"
-
-env -C $DIR "$JAVA_HOME/bin/java" -cp cultris2.jar c2settings
